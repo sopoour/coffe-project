b1aee957a3d01c16df836bf56031603ae4738e63
dianalemonche <dile18ab@student.cbs.dk>
2018-10-17 11:19:16 +0200

    push favorite.js

---
+++ scripts/favorite.js
@@ -0,0 +1,30 @@
+// 1. Click on button --> Store favorite choice
+var favorites
+[" "]
+
+// <button onclick="myFunction()">Click me</button>
+function addFavorite(favorites
+
+.
+push(
+    onclick = "addFavorite");
+[]
+
+)
+
+
+// 2. Favorite choice is stored in "favorites" array (new HTML page)
+
+var favorites
+[]
+
+// 3. Favorite choice is removed from favorite page
+
+// If coffee is in favorites, remove (if not, don't)
+
+function isFavorite()
+
+function removeFavorite()
+
+
+// If
