ea480d9f0870b9ea370b00e18bf8b736e079de39
dianalemonche <dile18ab@student.cbs.dk>
2018-10-17 12:42:00 +0200

    push favorite.js

--- scripts/favorite.js
+++ scripts/favorite.js
@@ -1,33 +1,37 @@
-function addFavorite() {
-    alert("Let's Create A Favorite");
-}
+
 // 1. Click on button --> Store favorite choice
-var favorites
-[" "]
+var favorites = [];
 
-// <button onclick="myFunction()">Click me</button>
-function addFavorite(favorites
+var selectedCoffee = [];
 
-.
-push(
-    onclick = "addFavorite");
-[]
+function addFavorite(selectedCoffee, favorites)
 
-)
+// If item not already in favorites (for loop), add to favorites array (new HTML page with array)
+// 2.1. Add "This has been added to your favorite"
+// 2.2. Display length of array & items of coffee
+{
+    var count = favorites.length;
+    for (var i = 0; i < count; i++) {
+        if (selectedCoffee [i] === favorites) {
+            favorites.push(selectedCoffee)
+        }
+        {
+            alert("You have chosen " + favorites.length + " favorite coffee(s")
+        }
 
+    else
+        alert("Hey there greedy! This has already been added to your favorites!")
 
-// 2. Favorite choice is stored in "favorites" array (new HTML page)
+    }
 
-var favorites
-[]
+}
 
 // 3. Favorite choice is removed from favorite page
 
 // If coffee is in favorites, remove (if not, don't)
 
-function isFavorite()
 
-function removeFavorite()
+function removeFavorite(selectedCoffee, favorites) {
 
+}
 
-// If
