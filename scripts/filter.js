aefa8ff59ecd341c0034c54413471f28edae2e0c
Jan <haehl.jan@gmail.com>
2018-10-31 17:21:30 +0100

    Dynamic filtering

--- scripts/filter.js
+++ scripts/filter.js
@@ -11,11 +11,12 @@ stores.push(store1, store2, store3);
 
 /*COFFEES*/
 //Generate some great coffees!
-addCoffee(store1,"Cappuchino",45);
-addCoffee(store2, "Cappuchino",40);
-addCoffee(store3,"Latte Macchiato",30);
-addCoffee(store3, "Espresso",14);
-addCoffee(store2, "Arabica",20);
+addCoffee(store1,"cappuccino",45);
+addCoffee(store2, "cappuccino",40);
+addCoffee(store3,"latte",30);
+addCoffee(store3, "espresso",14);
+addCoffee(store2, "arabica",20);
+
 /*var coffee1 = new Coffee(1,store1, "Cappuccino", 45);
 var coffee2 = new Coffee(2,store2, "Cappuccino", 40);
 var coffee3 = new Coffee(3,store1, "Latte Macchiato", 30);
@@ -36,48 +37,121 @@ for (var a in stores) {
 //FUNCTIONS
 
 var coffees=getCoffees();
+//alert("Wir haben "+coffees.length+" Kaffees im System.");
+
+function filter(dataType,operation,attributeValue,filterValue) {
+    //Returns true or false
+    /*
+    Accepted values for comparison: text -> equals // number -> smaller, smallerequals, equals, greaterequals, greater
+    If unaccepted input -> return false and alert error
+    */
+   if(dataType=="text") {
+       //alert("analyzieren wir den Typ");
+        if(filterValue===attributeValue) {
+            //alert("hier ist jetzt die situation: "+filterValue+" == "+attributeValue);
+            return 1;
+        }
+        else if(filterValue=="none"){
+            //alert("du hasch select ausgewählt, also gibts nix");
+            return 2;
+        }
+        
+        else {
+            //alert("Typ passt ned");
+            return 0;
+        }
+   }
+   else if(dataType=="number") {
+       //alert("analyzieren wir den Preis");
+        if(filterValue==0) {
+            return 2; //Kein Input
+        }
+           var comper;
+           switch(operation) {
+            case "equals":
+                comper = filterValue==attributeValue;
+                break;
+            case "smaller":
+                comper = filterValue<attributeValue;
+                break;
+            case "smallerequals":
+                comper = filterValue<=attributeValue;
+                break;
+            case "greaterequals":
+                //alert("hier ist jetzt die situation: "+filterValue+" >= "+attributeValue+" ??");
+                comper = filterValue>=attributeValue;
+                break;
+            case "greater":
+                comper = filterValue>attributeValue;
+                break;
+           }
+           if(comper) {
+               //lert("und das gibt true!");
+               return 1; //True
+           }
+           //alert("und das gibt false");
+    }
+    //alert("gibt 0 zurück, weil passt ned");
+    return 0; //Passt ned
+}
 
 function doFilter() {
     var filteredCoffees=[];
     var price = document.getElementById("priceInput").value;
-    var type = document.getElementById("typeInput").value;
+    var typeContainer = document.getElementById("typeInput");
+    var type = typeContainer.options[typeContainer.selectedIndex].value;
 
-    if (!price && type === "Select...") {
+    //alert("type is "+type);
+    if (!price && !type) {
         //Do stuff for no input values
-        alert("No Input values");
+        //alert("No Input values");
         //return means to cancel the function and get out of it again; you could also use false
         return null;
     }
 
-    for(var x=0;x<coffees.length;x++) {
-        var c = coffees[x];
-        //For what is the var lock?
-        /*
-        Otherwise it might be that coffees are pushed multiple times to filteredCoffees[].
-        Tough i am still working on a more generic way to do the filter (right now both filters are statically coded, want to have this generic)
-        */
-        var lock = false;
-        //Price Checker
-        if (c.price <= price && type === "Select...") {
-            filteredCoffees.push(c);
-            lock=true;
-        }
-        //Type Checker
-        if (c.type === type && !price) {
-            filteredCoffees.push(c);
-            lock=true;
-        }
+    var coffeeIndexes = [];
 
-        if (c.type === type && c.price <= price && !lock) {
-            filteredCoffees.push(c);
+    for(var x=0;x<coffees.length;x++) {
+        //alert(x+" schauen wir uns an, ist "+coffees[x].type+" und kostet "+coffees[x].price);
+        var filters = [];
+        filters.push(filter("number","greaterequals",coffees[x].price,price)); //Price
+        filters.push(filter("text","equals",coffees[x].type,type)); //Type
+
+        for(var a=0;a<filters.length;a++) {
+            if(filters[a]==1) {
+                //for all other filters
+                if(filters.length==1) {
+                    //Only one, so let's add it directly
+                    //alert("Füge index "+x+" hinzu...");
+                    filteredCoffees.push(coffees[x]);
+                    break;
+                }
+                else {
+                    var falseCounter=0;
+                    //Let's look at the other filter results
+                    for(var b=0;b<filters.length;b++) {
+                        if(filters[b]==0) falseCounter++;
+                    }
+                    //add only, if other filters are empty, but are not "wrong"/contradicting
+                    if(falseCounter==0) {
+                        //alert("Füge index "+x+" hinzu...");
+                        filteredCoffees.push(coffees[x]);
+                        break;
+                    }
+                }
+   
+            }
         }
+   
     }
-    alert
-    return filteredCoffees;
+
+    return filteredCoffees; 
 }
 
 
-//This function is for building a able
+
+
+//This function is for building a table
 
 function filterResult() {
     //call the actual filtering function
@@ -109,7 +183,7 @@ function filterResult() {
 
     //Init Header Row
     var columns = [];
-    columns.push("Type", "Price", "Store", "Button");
+    columns.push("Type", "Price", "Store");
     var row = table.insertRow(-1);
 
     //Fill out Header Row
@@ -120,20 +194,22 @@ function filterResult() {
     }
 
     //Coffee Rows
+
     for (var i = 0; i < filteredCoffees.length; i++) {
+        console.log("Kaffee "+i+" ist "+filteredCoffees[i].type);
         row = table.insertRow(-1);
 
-        var j = findCoffee(filteredCoffees[i]);
-        if(j<0) {
+        //var j = findCoffee(filteredCoffees[i]);
+        /*if(j<0) {
             alert("Großer Fehler");
             return null;
-        }
-        var buttonID = "coffee-"+j;
+        } */
+        var buttonID = "coffee-";
 
-        var b = document.createElement("BUTTON");
+        /*var b = document.createElement("BUTTON");
         b.setAttribute('class','btn coffeeSelect');
         b.setAttribute('id',buttonID);
-        b.innerHTML = 'Add To Favorites';
+        b.innerHTML = 'Add To Favorites';*/
 
         //"Hardcoded", should be smarter/more generic here, but need for a decision on how to iterate through data types in coffees[] (see above)
         var cell1 = row.insertCell(-1);
@@ -142,8 +218,6 @@ function filterResult() {
         cell2.innerHTML = filteredCoffees[i].price;
         var cell3 = row.insertCell(-1);
         cell3.innerHTML = filteredCoffees[i].store.name;
-        var cell4 = row.insertCell(-1);
-        cell4.appendChild(b);
 
     }
 
@@ -154,7 +228,7 @@ function filterResult() {
 
 }
 
-function findCoffee(coffee) {
+/*function findCoffee(coffee) {
     //get index of my coffee within the table
     for(var x=0;x<coffees.length;x++) {
         //search coffee and compare it will all related columns
@@ -163,5 +237,5 @@ function findCoffee(coffee) {
         }
     }
     return -1;
-}
+} */
 
