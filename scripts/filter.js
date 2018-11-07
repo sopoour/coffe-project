08f8eecaf25ed33648c5cbd4c95319741115889a
Jan <haehl.jan@gmail.com>
2018-11-07 13:41:52 +0100

    t

--- scripts/filter.js
+++ scripts/filter.js
@@ -205,8 +205,15 @@ function filterResult() {
         var b = document.createElement("BUTTON");
         b.setAttribute('class','btn coffeeSelect');
         b.setAttribute('id',buttonID);
-        b.setAttribute("onclick", addFavorite);
-        b.innerHTML = 'Add To Favorites';
+        
+        if(isFavorite==true) {
+            b.setAttribute("onclick", removeFavorite());
+            b.innerHTML = 'Remove From Favorites';
+        }
+        else {
+            b.setAttribute("onclick", addFavorite());
+            b.innerHTML = 'Add To Favorites';
+        }
 
         //"Hardcoded", should be smarter/more generic here, but need for a decision on how to iterate through data types in coffees[] (see above)
         var cell1 = row.insertCell(-1);
