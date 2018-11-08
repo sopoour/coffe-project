148952589ecdb639a2f1c169e490cdb2b13e7829
sopoour <36188360+sopoour@users.noreply.github.com>
2018-11-08 17:00:58 +0100

    commit by sophia

--- scripts/user.js
+++ scripts/user.js
@@ -6,38 +6,42 @@ var users = getUsers();
 
 var authUser = document.getElementById("loginSubmit");
 //the function shall be activated when user clicks on login button
-
-authUser.onclick = function () {
-    var userName = document.getElementById("userName");
-    var userPassword = document.getElementById("userPassword");
-    var loginResult = document.getElementById("loginResult");
-    //temporary index to check if my user exists or not
-    var tempIndex = -1;
-    //first check if either one of the inputs is empty
-    if (userName.value === "" || userPassword.value === "") {
-        loginResult.innerHTML = "<br> <br>" + "Please type in both username and password!";
-        //return false to get out of the function and wait for a new action
-        return false;
-    }
-    for (var i = 0; i < users.length; i++) {
-        // users[i].username/.password to check that the same user is compared
-        if (userName.value === users[i].username && userPassword.value === users[i].password) {
-            //if username and password match set tempIndex to it's actual index
-            tempIndex = i;
-            //store users in localStorage with the changed status of loggedIn
-            //since it is an object you need first stringify so that it actually shows everything in "value" what is inside of the object
-
-            //saves the user in local storage
-            saveUser(users[i]);
-            //open main filter page on same tab
-            window.location = "index.html";
+//try..catch function to ignore null error
+try {
+    authUser.onclick = function () {
+        var userName = document.getElementById("userName");
+        var userPassword = document.getElementById("userPassword");
+        var loginResult = document.getElementById("loginResult");
+        //temporary index to check if my user exists or not
+        var tempIndex = -1;
+        //first check if either one of the inputs is empty
+        if (userName.value === "" || userPassword.value === "") {
+            loginResult.innerHTML = "<br> <br>" + "Please type in both username and password!";
+            //return false to get out of the function and wait for a new action
+            return false;
         }
-    }
-    //this will only happen when the program checked all the users and tempIndex was never set to i but still set to initial -1
-    if (tempIndex === -1) {
-        loginResult.innerHTML = "<br> <br>" + "Your password or username is incorrect. Please try again!";
-    }
-};
+        for (var i = 0; i < users.length; i++) {
+            // users[i].username/.password to check that the same user is compared
+            if (userName.value === users[i].username && userPassword.value === users[i].password) {
+                //if username and password match set tempIndex to it's actual index
+                tempIndex = i;
+                //store users in localStorage with the changed status of loggedIn
+                //since it is an object you need first stringify so that it actually shows everything in "value" what is inside of the object
+
+                //saves the user in local storage
+                saveUser(users[i]);
+                //open main filter page on same tab
+                window.location = "index.html";
+            }
+        }
+        //this will only happen when the program checked all the users and tempIndex was never set to i but still set to initial -1
+        if (tempIndex === -1) {
+            loginResult.innerHTML = "<br> <br>" + "Your password or username is incorrect. Please try again!";
+        }
+    };
+} catch (err) {
+    console.log("Null caught");
+}
 
 /*
 try {
